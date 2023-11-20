0b25f6543c771383cac6ee86f85be7d909b3d36a
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-11-20 15:39:59 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -133,6 +133,25 @@ resource "aws_route_table" "app" {
   }
 }
 
+resource "aws_route_table" "db" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  route {
+    cidr_block                = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+  }
+
+
+  tags = {
+    Name = "db"
+  }
+}
+
 resource "aws_route" "default-route-table" {
   route_table_id            = var.default_route_table_id
   destination_cidr_block    = var.vpc_cidr
